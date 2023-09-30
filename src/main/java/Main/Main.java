5cabf2291691e6bd3f6c5f4f560e8d2d38fbe404
Raul_LA <rleonalvarez090@gmail.com>
2023-09-30 18:04:49 -0500

    clase main

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,18 @@
+
+package Main;
+import Vista.login;
+import Controlador.CLogin;
+
+public class Main {
+
+    public static login login;
+    public static CLogin cLogin;
+    
+    
+    public static void main(String[] args) {
+        
+        login = new login();
+        cLogin = new CLogin(login);
+    }
+    
+}
