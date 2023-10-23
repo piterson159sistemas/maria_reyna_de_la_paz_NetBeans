6fd0e23631718c4a9000ab994a961001957ad745
Raul_LA <rleonalvarez090@gmail.com>
2023-10-23 02:56:17 -0500

    Mejora frm Login

--- src/main/java/Vista/login.java
+++ src/main/java/Vista/login.java
@@ -6,7 +6,7 @@ package Vista;
 
 
 
-import Controlador.Consultas;
+import DAO.QueryLogin;
 import javax.swing.JOptionPane;
 
 
@@ -14,12 +14,6 @@ public class login extends javax.swing.JFrame {
 
     public login() {
         initComponents();
-        this.setTitle("Login");
-        this.setLocationRelativeTo(null);
-txtCod.setBackground(new java.awt.Color(0,0,0,1));
-txtClave.setBackground(new java.awt.Color(0,0,0,1));
-
-
     }
 
     /**
@@ -32,140 +26,89 @@ txtClave.setBackground(new java.awt.Color(0,0,0,1));
     private void initComponents() {
 
         jPanel2 = new javax.swing.JPanel();
-        btnCambiarClave = new javax.swing.JButton();
-        txtClave = new javax.swing.JPasswordField();
         txtCod = new javax.swing.JTextField();
+        txtClave = new javax.swing.JPasswordField();
+        btnCambiarClave = new javax.swing.JButton();
         btnIngresar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setLayout(null);
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btnCambiarClave.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtCod.setBackground(new java.awt.Color(204, 204, 204));
+        txtCod.setForeground(new java.awt.Color(0, 0, 0));
+        txtCod.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(204, 204, 204)));
+        jPanel2.add(txtCod, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 150, 160, 40));
+
+        txtClave.setBackground(new java.awt.Color(204, 204, 204));
+        txtClave.setForeground(new java.awt.Color(0, 0, 0));
+        txtClave.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(204, 204, 204)));
+        jPanel2.add(txtClave, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 210, 160, 40));
+
+        btnCambiarClave.setBackground(new java.awt.Color(21, 68, 122));
+        btnCambiarClave.setFont(new java.awt.Font("Corbel", 1, 14)); // NOI18N
+        btnCambiarClave.setForeground(new java.awt.Color(255, 255, 255));
         btnCambiarClave.setText("Cambiar contraseña");
-        btnCambiarClave.setBorder(null);
-        btnCambiarClave.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCambiarClaveActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnCambiarClave);
-        btnCambiarClave.setBounds(60, 700, 310, 60);
+        btnCambiarClave.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(204, 204, 204)));
+        jPanel2.add(btnCambiarClave, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, 140, 40));
 
-        txtClave.setBorder(null);
-        txtClave.setEchoChar('\u25cf');
-        txtClave.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtClaveActionPerformed(evt);
-            }
-        });
-        jPanel2.add(txtClave);
-        txtClave.setBounds(70, 490, 300, 60);
-
-        txtCod.setBorder(null);
-        jPanel2.add(txtCod);
-        txtCod.setBounds(70, 400, 300, 60);
-
-        btnIngresar.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        btnIngresar.setBackground(new java.awt.Color(21, 68, 122));
+        btnIngresar.setFont(new java.awt.Font("Corbel", 1, 14)); // NOI18N
+        btnIngresar.setForeground(new java.awt.Color(255, 255, 255));
         btnIngresar.setText("Ingresar");
-        btnIngresar.setBorder(null);
-        btnIngresar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnIngresarActionPerformed(evt);
-            }
-        });
-        jPanel2.add(btnIngresar);
-        btnIngresar.setBounds(70, 570, 280, 60);
+        btnIngresar.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(204, 204, 204)));
+        jPanel2.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 280, 140, 30));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/LO.png"))); // NOI18N
-        jPanel2.add(jLabel5);
-        jLabel5.setBounds(0, 0, 440, 790);
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 10)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Institucion Educativa Privada");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, -1, -1));
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 490, 800));
+        jLabel4.setFont(new java.awt.Font("Algerian", 1, 16)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("DE LA PAZ");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 70, 100, 20));
+
+        jLabel5.setFont(new java.awt.Font("Algerian", 1, 16)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("MARIA REYNA");
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, 120, 20));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon("D:\\Raul\\UTP\\Proyecto AED\\maria_reyna_de_la_paz_NetBeans\\src\\main\\java\\img\\user2.png")); // NOI18N
+        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, -1, -1));
+
+        jLabel7.setIcon(new javax.swing.ImageIcon("D:\\Raul\\UTP\\Proyecto AED\\maria_reyna_de_la_paz_NetBeans\\src\\main\\java\\img\\password.png")); // NOI18N
+        jPanel2.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, -1, -1));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon("D:\\Raul\\UTP\\Proyecto AED\\maria_reyna_de_la_paz_NetBeans\\src\\main\\java\\img\\logo.jpeg")); // NOI18N
+        jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 120));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon("D:\\Raul\\UTP\\Proyecto AED\\maria_reyna_de_la_paz_NetBeans\\src\\main\\java\\img\\fondooooooo.jpg")); // NOI18N
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 280, 410));
+
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 280, 410));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtClaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtClaveActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtClaveActionPerformed
-
-    private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
-
-       Consultas con = new Consultas();
-       con.consultarUsuario(txtCod.getText(),txtClave.getText());
-    
-    }//GEN-LAST:event_btnIngresarActionPerformed
-
-    private void btnCambiarClaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCambiarClaveActionPerformed
-      
-        this.setVisible(false);
-        recopilar_codigo_cambioContraseña RcopilarCodigoC = new recopilar_codigo_cambioContraseña();
-        RcopilarCodigoC.setVisible(true);
-    }//GEN-LAST:event_btnCambiarClaveActionPerformed
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new login().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton btnCambiarClave;
     public javax.swing.JButton btnIngresar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel2;
     public javax.swing.JPasswordField txtClave;
     public javax.swing.JTextField txtCod;
