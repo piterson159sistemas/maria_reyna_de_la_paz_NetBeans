54f57b637e2dac3cc2c5d76bb622adaee1451919
Raul_LA <rleonalvarez090@gmail.com>
2023-11-09 12:52:23 -0500

    algunas pruebas

--- src/test/java/Test/MainTest.java
+++ src/test/java/Test/MainTest.java
@@ -1,36 +1,78 @@
 
 package Test;
-import javax.swing.JFrame;
-import org.jdesktop.swingx.JXTreeTable;
-import org.jdesktop.swingx.treetable.DefaultMutableTreeTableNode;
-import org.jdesktop.swingx.treetable.DefaultTreeTableModel;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.Color;
 
 public class MainTest {
 
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("JXTreeTable Example");
+        JFrame frame = new JFrame("JTable with Colored Headers");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Crear un nodo raíz
-        DefaultMutableTreeTableNode root = new DefaultMutableTreeTableNode("Root");
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+        model.addColumn("Columna 1");
+        model.addColumn("Columna 2");
 
-        // Crear un modelo de tabla con una estructura de columna personalizada
-        DefaultTreeTableModel model = new DefaultTreeTableModel(root,
-                java.util.Arrays.asList("Nombre", "Valor", "Subcolumna 1", "Subcolumna 2"));
+        // Personaliza el renderizado de los encabezados
+        table.getTableHeader().setDefaultRenderer(new ColoredHeaderRenderer());
 
-        // Crear una JXTreeTable con el modelo
-        JXTreeTable treeTable = new JXTreeTable(model);
+        // Asigna un color a los encabezados
+        table.getTableHeader().setBackground(Color.YELLOW);
 
-        // Agregar filas y nodos para crear la estructura de la tabla
-        DefaultMutableTreeTableNode row1 = new DefaultMutableTreeTableNode(new Object[]{"Fila 1", "Valor 1", "Subvalor 1", "Subvalor 2"});
-        DefaultMutableTreeTableNode row2 = new DefaultMutableTreeTableNode(new Object[]{"Fila 2", "Valor 2", "Subvalor 3", "Subvalor 4"});
-        model.insertNodeInto(row1, root, root.getChildCount());
-        model.insertNodeInto(row2, root, root.getChildCount());
+        // Agrega algunos datos de ejemplo
+        model.addRow(new Object[]{"Dato 1", "Dato 2"});
+        model.addRow(new Object[]{"Dato 3", "Dato 4"});
 
-        frame.add(treeTable);
-        frame.pack();
+        // Agrega un ListSelectionListener para monitorear la selección de celdas
+        ListSelectionModel cellSelectionModel = table.getSelectionModel();
+        cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                    int selectedRow = table.getSelectedRow();
+                    int selectedColumn = table.getSelectedColumn();
+
+                    if (selectedRow >= 0 && selectedColumn >= 0) {
+                        String columnName = table.getColumnName(selectedColumn);
+                        Color headerColor = table.getTableHeader().getBackground();
+
+                        // Muestra un cuadro de diálogo con información
+                        JOptionPane.showMessageDialog(frame, "Color del encabezado de la columna: " + headerColor.toString() + "\nNúmero de columna: " + selectedColumn);
+                    }
+                }
+            }
+        });
+
+        frame.add(new JScrollPane(table));
+        frame.setSize(400, 300);
         frame.setVisible(true);
+    }
+
+    static class ColoredHeaderRenderer extends DefaultTableCellRenderer {
+        public ColoredHeaderRenderer() {
+            setHorizontalAlignment(SwingConstants.CENTER);
+            setForeground(Color.WHITE);  // Color del texto
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            // Personaliza el color del fondo del encabezado aquí
+            setBackground(Color.BLUE);
+
+            return this;
+        }
     }
     
 }
