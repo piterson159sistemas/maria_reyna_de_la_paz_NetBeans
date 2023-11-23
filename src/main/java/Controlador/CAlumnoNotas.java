e56f4d6ef336caa5b3c056ed2995ad5d3f446e40
Raul_LA <rleonalvarez090@gmail.com>
2023-11-23 12:05:51 -0500

    controlador Estudiante Notas

---
+++ src/main/java/Controlador/CAlumnoNotas.java
@@ -0,0 +1,72 @@
+package Controlador;
+
+import DAO.crudAlumnoNotas;
+import Procesos.ProcesosAlumnos;
+import Procesos.procesosAlumnoNotas;
+import VISTA_DIRECTIVO.Estudiante_Notas;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import javax.swing.JComboBox;
+
+
+public class CAlumnoNotas{
+
+    Estudiante_Notas vista;
+    procesosAlumnoNotas procesos = new procesosAlumnoNotas();
+    crudAlumnoNotas crudANotas = new crudAlumnoNotas();
+    ProcesosAlumnos procesosAux= new ProcesosAlumnos();
+    
+    public CAlumnoNotas(Estudiante_Notas estNotas){
+        vista=estNotas;
+        procesos.cargaInicialCombos(vista);
+        procesosAux.cargarNiveles(vista.cbxNivel);
+        ArrayList<String> periodos=crudANotas.buscarPeriodos();
+        procesos.cargarCombo(vista.cbxPeriodo, periodos);
+        procesosAux.CargarCombosEnlazados(vista.cbxNivel, vista.cbxGrado, vista.cbxArea);
+        procesosAux.CargarCombosEnlazadosAreas( vista.cbxArea,vista.cbxComp);
+        CargarNombreCompetencia(vista.cbxComp);
+        CargarAreaPeriodo(vista.cbxArea, vista.cbxPeriodo);
+    }
+    
+    public void CargarNombreCompetencia(JComboBox cbxComp){ 
+        
+        cbxComp.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange()==ItemEvent.SELECTED){
+                    String CompSelect = cbxComp.getSelectedItem().toString();
+                    vista.lblNomCompetencia.setText(CompSelect);
+                }
+            }
+        });
+
+    }
+    
+    public void CargarAreaPeriodo(JComboBox cbxArea,JComboBox cbxPeriodo){ 
+        
+        cbxArea.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange()==ItemEvent.SELECTED){
+                    String AreaSelect = cbxArea.getSelectedItem().toString();
+                    vista.lblCurso.setText("Curso: "+AreaSelect);
+                }
+            }
+        });
+        
+        cbxPeriodo.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange()==ItemEvent.SELECTED){
+                    String periodoSelect = cbxPeriodo.getSelectedItem().toString();
+                    String infArea=vista.lblNomCompetencia.getText();
+                    vista.lblPeriodo.setText("Periodo: "+periodoSelect);
+                    
+                }
+            }
+        });
+    }
+
+    
+}
