a9ee9ee16f789867a1319dae8ac0bcd544bfad07
Raul_LA <rleonalvarez090@gmail.com>
2023-12-08 17:50:27 -0500

    algunas pruebas

--- src/test/java/Test/MainTest.java
+++ src/test/java/Test/MainTest.java
@@ -2,61 +2,39 @@
 package Test;
 
 import javax.swing.*;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.text.NumberFormat;
 
 public class MainTest {
-
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainTest().createAndShowGUI());
-    }
+        SwingUtilities.invokeLater(() -> {
+            // Mostrar el cuadro de diálogo de entrada
+            String input = showNumericInputDialog("Ingrese un número:");
 
-    private void createAndShowGUI() {
-        JFrame frame = new JFrame("JLabel Encima de JComboBox");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-
-        JComboBox<String> comboBox1 = new JComboBox<>(new String[]{"Mayor", "Menor"});
-        JComboBox<String> comboBox2 = new JComboBox<>(new String[]{"Opción 1", "Opción 2", "Opción 3"});
-
-        JLabel label = new JLabel("Etiqueta sobre JComboBox");
-        label.setForeground(Color.RED);
-
-        // Configurar la posición y el tamaño del JComboBox y JLabel
-        comboBox1.setPreferredSize(new Dimension(100, 30));
-        comboBox2.setPreferredSize(new Dimension(100, 30));
-
-        // Agregar ActionListener al JComboBox para ajustar la visibilidad del otro JComboBox
-        comboBox1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String selectedOption = (String) comboBox1.getSelectedItem();
-                ajustarVisibilidadComboBox(selectedOption, comboBox2);
+            // Mostrar el resultado
+            if (input != null) {
+                JOptionPane.showMessageDialog(null, "Número ingresado: " + input);
             }
         });
-
-        panel.add(comboBox1);
-        panel.add(comboBox2);
-        panel.add(label);
-
-        frame.getContentPane().add(panel);
-        frame.setSize(250, 180);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
     }
 
-    private void ajustarVisibilidadComboBox(String selectedOption, JComboBox<String> comboBox) {
-        if ("Mayor".equals(selectedOption)) {
-            // Ocultar el JComboBox
-            comboBox.setVisible(false);
-        } else if ("Menor".equals(selectedOption)) {
-            // Mostrar el JComboBox
-            comboBox.setVisible(true);
+    private static String showNumericInputDialog(String message) {
+        NumberFormat format = NumberFormat.getIntegerInstance();
+        NumberFormatter formatter = new NumberFormatter(format);
+        formatter.setValueClass(Integer.class);
+        formatter.setMinimum(0); // Puedes ajustar el valor mínimo según tus necesidades
+        formatter.setMaximum(Integer.MAX_VALUE); // Puedes ajustar el valor máximo según tus necesidades
+
+        JFormattedTextField textField = new JFormattedTextField(formatter);
+        textField.setColumns(10); // Puedes ajustar el tamaño del campo de texto según tus necesidades
+
+        int result = JOptionPane.showConfirmDialog(null, textField, message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        if (result == JOptionPane.OK_OPTION) {
+            return textField.getText();
+        } else {
+            return null;
         }
     }
 }
