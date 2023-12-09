785c87ce768a50d292542813705c0e4b7f3ef629
Raul_LA <rleonalvarez090@gmail.com>
2023-12-08 22:26:26 -0500

    (+) metodo iniciarSesion

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,21 +1,19 @@
 
 package Main;
-import Controlador.CListadoAlumDirec;
 import VISTA_INICIARSESION.login;
 import Controlador.CLogin;
-import Vista.lista_estudiantes_directivo_1;
 
 public class Main {
 
-    public static login login;
-    public static CLogin cLogin;
-    
+    public static void iniciarSesion(){
+        login login = new login();
+        CLogin cLogin = new CLogin(login);
+        
+    }
     
     public static void main(String[] args) {
         
-        login = new login();
-        cLogin = new CLogin(login);
-        
+        iniciarSesion();    
 
         /* Para inicar sesion rapido cuando hago pruebas
         login.txtCod.setText("P0101231");
